d334f9261cc4202745444d57e805e2121fb3506d
samod singh <samod0098@gmail.com>
2021-08-17 11:28:27 +0530

    udemy-first-part

---
+++ myScript.js
@@ -0,0 +1,37 @@
+// let massMark = 78;
+// let heightMark =1.95;
+// let massJohn = 92;
+// let heihtJohn = 1.95;
+ 
+// let BMIMark = massMark/heightMark **2;
+// let BMIjohn = massJohn /(heihtJohn * heihtJohn);
+// console.log(BMIMark, BMIjohn);
+//   let firstName = "jonas"; 
+//   let job = "teacher";
+//   let birthYear = 1993;
+//   let year = 2037;
+//   let jonas ="I' am" +" "+ firstName + ",a "+(year -birthYear)+"years old"+job+"!";
+//   console.log(jonas);
+
+//   let jonasNew =`I' m ${firstName} a ${year - birthYear}year old ${job}!`;
+//   console.log(jonasNew); 
+// let age = 15;
+// if(age >= 18){
+//     console.log('Sarch can start driving lincese');
+// }else{
+//     let yearsLaft =18-age;
+//     console.log(`sarah is too young. Wait another ${yearsLaft}years:s`);
+// }
+
+// let inputYear = '1991';
+// console.log(Number(inputYear),inputYear);
+// console.log(Number( inputYear)+18);
+
+// console.log(Number('samod'));
+// console.log(typeof NaN);
+// console.log(String(23),23);
+
+
+
+
+
